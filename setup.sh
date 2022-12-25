673324ce23a35ba4de6c7f84d92bcd8dedcf6b2c
AondonaMoses <119983638+AondonaMoses@users.noreply.github.com>
2022-12-25 17:56:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
-mkdir -p ~/.streamlit echo "[server] headless = true port = $PORT enableCORS = false " > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
